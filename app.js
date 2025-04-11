891b0538e5aaa14b94e82b49ad9077d27a48ebd8
alexmararg <alexmar.informatic@gmail.com>
2025-04-11 19:36:44 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const par = document.getElementById("excuse");
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+
+let action = ['ate', 'peed', 'crushed', 'broke'];
+
+let what = ['my homework', 'my phone', 'the car'];
+
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function generateNumber(va){
+		return Math.floor(Math.random() * va.length) ;
+        console.log("");
+}
+
+function generateExcuse(){
+    let exc=who[generateNumber(who)]+" "+action[generateNumber(action)]+" "+what[generateNumber(what)]+" "+when[generateNumber(when)];
+    par.innerHTML = exc;
+}
+
+generateExcuse();
